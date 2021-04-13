2728cae0fae176871768dec21dc02808a502ba7c
mtndaghyani <matin.daghyani@gmail.com>
2021-04-14 03:23:24 +0430

    Go back to home after resource found

--- classes/agent.py
+++ classes/agent.py
@@ -11,6 +11,7 @@ class Agent:
         self.game = None
         self.local_map = None
         self.path_to_follow = []
+        self.home_path = []
         self._targets = {"RESOURCE": lambda cell: cell.resource_value > 0,
                          "HOME": lambda cell: cell.x == self.game.baseX and cell.y == self.game.baseY,
                          "NEAREST_INVISIBLE": lambda cell: cell.type == -1}
@@ -27,3 +28,16 @@ class Agent:
     def get_random_direction():
         return [Direction.DOWN, Direction.UP, Direction.RIGHT, Direction.LEFT][randint(0, 3)]
 
+    def set_home_path(self, path):
+        temp = path[::-1]
+        reversed_path = []
+        for direction in temp:
+            if direction.value == 1:
+                reversed_path.append(Direction.LEFT)
+            elif direction.value == 2:
+                reversed_path.append(Direction.DOWN)
+            elif direction.value == 3:
+                reversed_path.append(Direction.RIGHT)
+            else:
+                reversed_path.append(Direction.UP)
+        self.home_path = reversed_path
--- classes/kargar_agent.py
+++ classes/kargar_agent.py
@@ -7,13 +7,14 @@ class KargarAgent(Agent):
         if len(self.path_to_follow) > 0:
             return self.path_to_follow.pop(0)
         if self.game.ant.currentResource.value > 0:
-            # TODO
-            print("Where to go?")
-            return Direction.UP
+            print("Go home!")
+            self.path_to_follow = self.home_path
+            return self.get_answer()
         else:
             path = self.local_map.get_path_to(self._targets.get(Target.RESOURCE))
             if path is not None:
                 self.path_to_follow = path
+                self.set_home_path(path)
                 print("Resource found!")
                 return self.get_answer()
             path = self.local_map.get_path_to(self._targets.get(Target.NEAREST_INVISIBLE))
@@ -21,7 +22,6 @@ class KargarAgent(Agent):
                 self.path_to_follow = path
                 print("New invisible found!")
                 return self.get_answer()
-            self.path_to_follow = [Direction.CENTER]
             print("PATH NOT FOUND!")
-            return Direction.DOWN
+            return KargarAgent.get_random_direction()
 
